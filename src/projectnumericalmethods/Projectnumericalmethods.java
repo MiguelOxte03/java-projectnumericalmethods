d682fe8e9f27abb4d58e2bac0e713b4298de4386
Dell <Dell@Gaspar>
2020-02-14 12:19:25 -0600

    se anadieron las variables necesarias para los calculos de errores, en le constructor que se acaba de agregar tambien, a si mismo se inicializaron los bottones y label para la interfaz grafica

--- src/projectnumericalmethods/Projectnumericalmethods.java
+++ src/projectnumericalmethods/Projectnumericalmethods.java
@@ -3,11 +3,36 @@ package projectnumericalmethods;
 import java.awt.*;
 import javax.swing .*;
 import java.awt.event.*;
-public class Projectnumericalmethods {
+public class Projectnumericalmethods extends JFrame{
+    private JRadioButton relativo,absoluto;
+    private JButton redondear,truncar,limpiar;
+    private JLabel res,vreal,vaproxi,numcifr;
+    private JTextField valorreal,valoraproximado,resultado,numcifras;
+    JPanel este=new JPanel();
+    JPanel oeste=new JPanel();
+    private int errorelejido=0;
+    private int numerodecifras=0;
+    int divisor=1;
+    
+    private double tenervalorreal;
 
-    /**
-     * @param args the command line arguments
-     */
+    public Projectnumericalmethods(){
+        res =new JLabel("Resultado obtenido");
+        vreal =new JLabel("Valor real");
+        vaproxi =new JLabel("Valor aproximado");
+        valorreal=new JTextField(20);
+        valoraproximado=new JTextField(20);
+        resultado=new JTextField(20);
+        relativo=new JRadioButton("Relativo");
+        absoluto=new JRadioButton("absoluto");
+        numcifr=new JLabel("Numero de cifras");
+        numcifras=new JTextField(20);
+        redondear=new JButton("Redondeo");
+        truncar=new JButton("Truncar");
+        limpiar=new JButton("Limpiar");
+    }
+    
+    ButtonGroup absoorela= new ButtonGroup();
     public static void main(String[] args) {
         // TODO code application logic here
     }
