c6708c9e458827d79a81a6629975fc36f072e440
Dell <Dell@Gaspar>
2020-02-14 12:33:04 -0600

    se aniadio el controlador de eventos para que cada vez que suceda algo en los botones o en los radiobuton el programa acualice la informacion que tiene, asi mismo se le indico a los otones que el controlador es un objeto llamado acciones del tipo manejador eventos

--- src/projectnumericalmethods/Projectnumericalmethods.java
+++ src/projectnumericalmethods/Projectnumericalmethods.java
@@ -3,6 +3,9 @@ package projectnumericalmethods;
 import java.awt.*;
 import javax.swing .*;
 import java.awt.event.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.InputMismatchException;
 public class Projectnumericalmethods extends JFrame{
     private JRadioButton relativo,absoluto;
     private JButton redondear,truncar,limpiar;
@@ -31,6 +34,8 @@ public class Projectnumericalmethods extends JFrame{
         truncar=new JButton("Truncar");
         limpiar=new JButton("Limpiar");
         
+        manejareventos acciones=new manejareventos();
+        
         este=new JPanel();
         este.setLayout(new GridLayout(8,1));
         este.add(vreal);
@@ -56,7 +61,9 @@ public class Projectnumericalmethods extends JFrame{
         botones.add(redondear);
         botones.add(truncar);
         botones.add(limpiar);
-        
+        redondear.addActionListener(acciones);
+        truncar.addActionListener(acciones);
+        limpiar.addActionListener(acciones);
         
         Container paneles=new Container();
         paneles=getContentPane();
@@ -69,5 +76,85 @@ public class Projectnumericalmethods extends JFrame{
     public static void main(String[] args) {
         // TODO code application logic here
     }
+     private class manejareventos implements ActionListener{
+        public void actionPerformed(ActionEvent accion){
+        	if(accion.getSource()==redondear){
+        		try{	
+        			BigDecimal numero=new BigDecimal(Double.parseDouble(valorreal.getText()));
+        			numero=numero.setScale(Integer.parseInt(numcifras.getText()),RoundingMode.HALF_UP);
+        			
+        			
+        			if(errorelejido==1){
+        				double devuelto=numero.doubleValue();
+        				double resultadototal=funcionrelativo(devuelto);
+        				valoraproximado.setText(String.valueOf(numero.doubleValue()));resultado.setText(String.valueOf(resultadototal));
+        			}
+        			else{
+        				double devuelto=numero.doubleValue();
+        				double resultadototal=funcionabsoluto(devuelto);
+        				valoraproximado.setText(String.valueOf(numero.doubleValue()));resultado.setText(String.valueOf(resultadototal));
+        			}
+        		}catch(InputMismatchException e){
+        			JOptionPane.showMessageDialog(null,"los datos deben ser numericos");
+        		}catch(Exception e){
+        			JOptionPane.showMessageDialog(null,"los campos estan vacios"+e);
+        		}
+        	}
+        	//
+        	if(accion.getSource()==truncar){
+        		try{
+        			//
+        			double cambio1=Double.parseDouble(valorreal.getText());
+        			double redondeo=cambio1%1;
+        			int vueltas=Integer.parseInt(numcifras.getText());
+        			
+        			for(int n=0;n<vueltas;n++){
+        				divisor*=10;
+        			}
+        			
+        			double truncamiento=divisor*redondeo-((divisor*redondeo)%1);        			
+        			cambio1=(cambio1-redondeo);
+        			double negro=truncamiento/divisor;
+        			double valortruncado=cambio1+negro;
+        			
+        			
+        			if(errorelejido==1){
+        				double resultadototal=funcionrelativo(valortruncado);
+        				valoraproximado.setText(String.valueOf(valortruncado));resultado.setText(String.valueOf(resultadototal));
+        			}	else{
+        				double resultadototal=funcionabsoluto(valortruncado);
+        				valoraproximado.setText(String.valueOf(valortruncado));resultado.setText(String.valueOf(resultadototal));
+        			}
+        		}catch(InputMismatchException e){
+        			JOptionPane.showMessageDialog(null,"los datos deben ser numericos");
+        		}catch(Exception e){
+        			JOptionPane.showMessageDialog(null,"los campos estan vacios"+e);
+        		}
+        	}
+        	if(relativo.isSelected()){
+        		errorelejido=1;
+        	}
+        	if(absoluto.isSelected()){
+        		errorelejido=2;
+        	}
+        	if(accion.getSource()==limpiar){
+        		valoraproximado.setText("");
+        		valorreal.setText("");
+        		numcifras.setText("");
+        		resultado.setText("");
+        	}
+        }   
+    }
+     double funcionrelativo(double absoluto){
+    		tenervalorreal=Double.parseDouble(valorreal.getText());
+    		double resultadof=tenervalorreal-absoluto;
+    		return resultadof/tenervalorreal;
+    		
+		}
+    	double funcionabsoluto(double resultado){
+    		tenervalorreal=Double.parseDouble(valorreal.getText());
+    		double resultadof=tenervalorreal-resultado;
+    		return resultadof;
+    	}
     
 }
